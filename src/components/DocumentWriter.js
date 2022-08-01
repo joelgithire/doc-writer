6323d3fa644723dae17d64b93e417261503e172a
joelgithire <jlmukono@gmail.com>
2022-08-01 15:55:35 +0300

    add input area

--- src/components/DocumentWriter.js
+++ src/components/DocumentWriter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./DocumentWriter.css";
 
 function DocumentWriter() {
+  const placeHolderText = "Hello world";
   return (
     <div>
-      <textarea></textarea>
+      <input className="document-area" value="" />
     </div>
   );
 }
