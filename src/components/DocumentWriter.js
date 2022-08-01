eec470428de03ef95ae979b3d3bd9bb9866f2a0c
joelgithire <jlmukono@gmail.com>
2022-08-01 22:42:13 +0300

    test

--- src/components/DocumentWriter.js
+++ src/components/DocumentWriter.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./DocumentWriter.css";
 
 function DocumentWriter() {
-  const placeHolderText = "Hello world";
   return (
     <div>
       <input className="document-area" value="Joshua" />
